0307d6a2dbc1cbdacecaf23e84f62ae35b36cf38
Pavel Reznikov <pashka.reznikov@gmail.com>
2022-06-01 23:06:03 -0700

    sentry setup

--- chalice_utils/sentry.py
+++ chalice_utils/sentry.py
@@ -4,7 +4,16 @@ import sentry_sdk
 from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 
 
+_sentry_setup = False
+
+
 def setup_sentry(dsn: Optional[str]):
+    global _sentry_setup
+    if _sentry_setup:
+        return
+
+    _sentry_setup = True
+
     if dsn:
         sentry_sdk.init(
             dsn=dsn,
